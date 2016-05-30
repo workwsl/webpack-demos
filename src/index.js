fdaa691ff3d270f18824dca437534a6a22c766cb
flow <workwsl@163.com>
2016-05-30 17:46:00 +0800

    webpack-demos

---
+++ src/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class HelloWorld extends Component {
+  render(){
+    return (
+      <h1>Hello world</h1>
+    )
+  }
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
